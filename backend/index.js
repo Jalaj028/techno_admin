9216cd515071485ebda5426b78e09311ab1a53c6
Jalaj2004 <jalajbhatt2004@gmail.com>
2024-05-07 11:10:22 +0530

    deployment fixes

--- backend/index.js
+++ backend/index.js
@@ -13,8 +13,8 @@ const { Event } = require("./db/index");
 app.use(bodyParser.json());
 app.use(cors(
     {
-        origin:["https://techno-admin-xzj6.vercel.app"],
-        methods:["GET", "POST"],
+        origin:["https://techno-admin-xzj6.vercel.app", "http://127.0.0.1:5173/"],
+        methods:["GET", "POST", "PUT", "DELETE"],
         credentials: true
     }
 ));
